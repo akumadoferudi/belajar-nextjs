151db4f587926736f4bab80787177538b4a30701
akumadoferudi <ferdiansyahachmad29@gmail.com>
2024-12-12 09:40:21 +0700

    feat: tugas hari-9

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string;
-};
+// type Data = {
+//   name: string;
+// };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<any>,
+  // res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: "John Doe" });
+  // res.status(200).json({ name: "John Doe" });
+  try {
+    const response = await (await fetch("https://dummyjson.com/users")).json();
+    // console.log("response => ", response);
+    res.status(200).json({ ...response });
+  } catch (err) {
+    console.log("error => ", err);
+    res.status(500).json({ "error" : err });
+  }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
-import Head from "next/head";
+// import Head from "next/head";
 import Layout from "@/layout";
-import Content from "@/components/content";
+// import Content from "@/components/content";
+import { useEffect } from "react";
 
-interface ContentElementProps {
-  children?: any;
-  metaTitle?: string;
-  metaDescription?: any;
-}
+// interface ContentElementProps {
+//   children?: any;
+//   metaTitle?: string;
+//   metaDescription?: any;
+// }
 
 export default function Main() {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((res) => console.log("response => ", res))
+      .catch((err) => console.log("error => ", err));
+  }, []);
+
   return (
     <>
       <Layout metaTitle="Main Page" metaDescription="Ja, this is index page">
